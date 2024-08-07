10e5192041a6c6ad317c0be40b744700fd021c0d
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-08-07 23:21:32 +0300

    place buttons

---
+++ index.js
@@ -0,0 +1,23 @@
+// const firstNum = document.querySelector()
+// const secondNum = document.querySelector()
+// const operator = document.querySelector()
+
+function operate(firstNum, secondNum, operator) {
+  return operator(firstNum, secondNum);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
